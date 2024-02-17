06ad3e70d71da01dafa6815c5fbf5c8c525d7523
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-17 20:41:29 +0300

    move auditing to new project

--- src/main/java/com/colak/springjpatutorial/auditing/README.md
+++
@@ -1,3 +0,0 @@
-The original idea is from
-https://medium.com/@vpaipathirana/jpa-auditing-spring-boot-spring-security-575c77867570
-
--- src/main/java/com/colak/springjpatutorial/auditing/repository/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.colak.springjpatutorial.auditing.repository;
-
-import com.colak.springjpatutorial.auditing.jpa.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier,Long> {
-}
