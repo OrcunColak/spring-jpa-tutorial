3171590e41c57b082be43aaea0f7864767c15192
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-24 09:22:11 +0300

    move hibernate formula annotation to new project

--- src/main/java/com/colak/springjpatutorial/hibernate/formula/repository/AuthorFormulaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.colak.springjpatutorial.hibernate.formula.repository;
-
-import com.colak.springjpatutorial.hibernate.formula.jpa.AuthorFormula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorFormulaRepository extends JpaRepository<AuthorFormula,Long> {
-}
